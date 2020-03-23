7c26a1753f20b397b875e18e2032368350c28da4
Hassan78 <hassan.ali@g.austincc.edu>
2020-03-23 12:24:39 -0500

    added theme on book list

--- src/components/booklist.jsx
+++ src/components/booklist.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/themcontext'
 
 class BookList extends Component {
-    state = {  }
-    render() { 
+    state = {}
+    render() {
         return (
-            <div className="book_list">
-                <ul>
-                    <li>Once upon a time</li>
-                    <li>Pride and Prejudice</li>
-                    <li>The Sun Also Raises</li>
-                </ul>
-            </div>
-         );
+            <ThemeContext.Consumer>{(context) => {
+                const{isLightTheme,light,dark} = context;
+                const theme = isLightTheme?light:dark;
+
+                return (
+                    <div className="book_list" style={{background:theme.bg,color:theme.syntax}}>
+                        <ul>
+                            <li>Once upon a time</li>
+                            <li>Pride and Prejudice</li>
+                            <li>The Sun Also Raises</li>
+                        </ul>
+                    </div>
+                )
+            }}
+
+            </ThemeContext.Consumer>
+        );
     }
 }
- 
+
 export default BookList;
--- src/contexts/themcontext.jsx
+++ src/contexts/themcontext.jsx
@@ -4,7 +4,7 @@ export const ThemeContext = createContext();
 
 class ThemeContextProvider extends Component {
     state = { 
-        isLightTheme:true,
+        isLightTheme:false,
         light:{syntax:'#0a0909',ui:'#b5b1b1',bg:'#617ded'},
         dark:{syntax:'#edf4f2',  ui:'#0a0909',   bg:'#999191'}
 
